1b1ab9428eed492f160b16fdc11726e49cb39647
Camilly Lemos Duarte <camilly.duarte@cwi.com.br>
2023-07-15 20:32:53 -0300

    script

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
-const express = require('express')
-const { resolve } = require('path')
-const path = require('path');
-const app = express()
+// const express = require('express')
+// const { resolve } = require('path')
+// const path = require('path');
+// const app = express()
 
-//Estáticos -> Roteamento dos Componentes
-// app.use('/',
-//   express.static(
-//     resolve(
-//       __dirname,
-//       './build'
-//     )
-//   )
-// ) 
+// //Estáticos -> Roteamento dos Componentes
+// // app.use('/',
+// //   express.static(
+// //     resolve(
+// //       __dirname,
+// //       './build'
+// //     )
+// //   )
+// // ) 
 
-//  app.get('/*', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
-// })
+// //  app.get('/*', (req, res) => {
+// //   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// // })
 
-app.listen(process.env.PORT || 3000, (err) => {
-  err ? console.log("err", err) : console.log("Tudo Funcionando!")
-});
+// app.listen(process.env.PORT || 3000, (err) => {
+//   err ? console.log("err", err) : console.log("Tudo Funcionando!")
+// });
